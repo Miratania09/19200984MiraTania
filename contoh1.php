7d8079d58e5b56ab5496eced582be532463f0871
Miratania09 <90584001+Miratania09@users.noreply.github.com>
2021-09-21 17:10:31 +0700

    Commit changes

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Mira Tania
+ Saya tinggal di daerah Cilandak
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
